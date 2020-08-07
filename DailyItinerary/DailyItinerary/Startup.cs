30e6c4bb20a29c7027fc4da37565a5e45627c6f8
Haseeb <haseeb01001@gmail.com>
2020-08-07 19:23:29 -0400

    Runnable

--- DailyItinerary/DailyItinerary/Startup.cs
+++ DailyItinerary/DailyItinerary/Startup.cs
@@ -63,7 +63,8 @@ namespace DailyItinerary
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+                    //spa.UseReactDevelopmentServer(npmScript: "start");
                 }
             });
         }
